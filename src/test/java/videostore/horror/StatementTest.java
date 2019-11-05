0e75bf3f04ec5519b7f2b5bb9b8019a8c14f1028
Kristof Neirynck <k.neirynck@gmail.com>
2019-11-05 21:52:25 +0100

    The test no longer truly tests the Customer class

---
+++ src/test/java/videostore/horror/StatementTest.java
@@ -0,0 +1,29 @@
+package videostore.horror;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class StatementTest {
+
+    @Test
+    public void characterizationTest() {
+        final List<Rental> rentals = Arrays.asList(
+                new Rental(new Movie("Star Wars", Movie.Type.NEW_RELEASE), 6),
+                new Rental(new Movie("Sofia", Movie.Type.CHILDRENS), 7),
+                new Rental(new Movie("Inception", Movie.Type.REGULAR), 5));
+
+        String expected = "Rental Record for John Doe\n"
+                + "	Star Wars	18.0\n"
+                + "	Sofia	7.5\n"
+                + "	Inception	6.5\n"
+                + "Amount owed is 32.0\n"
+                + "You earned 4 frequent renter points";
+        
+        assertEquals(expected, new Statement("John Doe", rentals).toString());
+    }
+}
