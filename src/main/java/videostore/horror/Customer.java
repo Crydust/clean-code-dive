672298669917f28a40ad30659d64244d3cab3471
Kristof Neirynck <k.neirynck@gmail.com>
2019-11-05 21:41:37 +0100

    Make methods static

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Objects;
 
 class Customer {
-    private final String name;
+    private final String customerName;
     private final List<Rental> rentals = new ArrayList<>();
 
-    public Customer(String name) {
-        this.name = name;
+    public Customer(String customerName) {
+        this.customerName = customerName;
     }
 
     public void addRental(Rental rental) {
@@ -19,33 +19,37 @@ class Customer {
     }
 
     public String statement() {
-        String result = formatHeader();
+        return statement(customerName, rentals);
+    }
+
+    public static String statement(String customerName, List<Rental> rentals) {
+        String result = formatHeader(customerName);
         for (Rental rental : rentals) {
             result += formatBodyLine(rental);
         }
         result += formatFooter(
-                calculateTotalPrice(),
-                calculateTotalFrequentRenterPoints());
+                calculateTotalPrice(rentals),
+                calculateTotalFrequentRenterPoints(rentals));
         return result;
     }
 
-    private double calculateTotalPrice() {
+    private static double calculateTotalPrice(List<Rental> rentals) {
         return rentals.stream()
                     .mapToDouble(Rental::calculatePrice)
                     .sum();
     }
 
-    private int calculateTotalFrequentRenterPoints() {
+    private static int calculateTotalFrequentRenterPoints(List<Rental> rentals) {
         return rentals.stream()
                 .mapToInt(Rental::calculateFrequentRenterPoints)
                 .sum();
     }
 
-    private String formatHeader() {
-        return "Rental Record for " + name + "\n";
+    private static String formatHeader(String customerName) {
+        return "Rental Record for " + customerName + "\n";
     }
 
-    private String formatBodyLine(Rental rental) {
+    private static String formatBodyLine(Rental rental) {
         return "\t" + rental.getMovie().getTitle() + "\t"
                 + rental.calculatePrice() + "\n";
     }
