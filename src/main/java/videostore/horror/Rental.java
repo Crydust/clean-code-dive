fb7db7c96564e5b9d47445fc10170a1500a37ee4
Kristof Neirynck <k.neirynck@gmail.com>
2019-11-05 20:47:10 +0100

    Inline getters

--- src/main/java/videostore/horror/Rental.java
+++ src/main/java/videostore/horror/Rental.java
@@ -17,16 +17,16 @@ public class Rental {
         switch (movie.getPriceCode()) {
             case REGULAR:
                 price += 2;
-                if (getDaysRented() > 2)
-                    price += (getDaysRented() - 2) * 1.5;
+                if (daysRented > 2)
+                    price += (daysRented - 2) * 1.5;
                 break;
             case NEW_RELEASE:
-                price += getDaysRented() * 3;
+                price += daysRented * 3;
                 break;
             case CHILDRENS:
                 price += 1.5;
-                if (getDaysRented() > 3)
-                    price += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3)
+                    price += (daysRented - 3) * 1.5;
                 break;
         }
         return price;
